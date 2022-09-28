5b49586af349fc37b02b960571afc53c795fd828
wm <958319520@qq.com>
2022-09-28 16:06:59 +0800

    update mail display content

--- src/MailBox.py
+++ src/MailBox.py
@@ -2,6 +2,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
+
 class MailBox:
 
     def __init__(self, smtp_server, sender_account, sender_passwd, myLogger):
@@ -20,11 +21,20 @@ class MailBox:
             self.myLogegr.info('邮箱登录失败')
             smtp.quit()
 
+    def connServerBySSL(self):
+        try:
+            smtp = smtplib.SMTP_SSL(self.smtp_server, 465)
+            smtp.login(self.sender_account, self.sender_passwd)
+            self.smtp = smtp
+        except smtplib.SMTPException:
+            self.myLogegr.info('邮箱登录失败')
+            smtp.quit()
+
     def sendMessage(self, sender_addr, receiver_addr, subject, content):
         smtp = self.smtp
         msg = MIMEText(content)
-        msg['From'] = sender_name
-        msg['To'] = receiver_name
+        msg['From'] = '\"打卡提示机器人\" <' + sender_addr + '>'
+        msg['To'] = '\"老大\" <' + receiver_addr + '>'
         msg['Subject'] = Header(subject, 'utf-8')
         try:
             smtp.sendmail(sender_addr, receiver_addr, msg.as_string())
